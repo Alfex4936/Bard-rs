f64cc812a961065d44d07779c4385a1dc0d4a09f
Alfex4936 <ikr@kakao.com>
2023-05-16 14:03:24 +0900

    New Bard model
    20230514

--- src/main.rs
+++ src/main.rs
@@ -102,7 +102,7 @@ impl Chatbot {
         );
 
         let encoded: String = form_urlencoded::Serializer::new("https://bard.google.com/_/BardChatUi/data/assistant.lamda.BardFrontendService/StreamGenerate?".to_string())
-            .append_pair("bl", "boq_assistant-bard-web-server_20230507.20_p2")
+            .append_pair("bl", "boq_assistant-bard-web-server_20230514.20_p0")
             .append_pair("_reqid", &self.reqid.to_string())
             .append_pair("rt", "c")
             .finish();
